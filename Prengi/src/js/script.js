bf7818555b8f2ca5a9c0d1dc64c9ac159f727438
Pavel Prokopenya <prokopenya.work@gmail.com>
2022-04-25 17:16:07 +0300

    different screen sizes' adaptations ready

--- Prengi/src/js/script.js
+++ Prengi/src/js/script.js
@@ -17,4 +17,23 @@ $(document).ready(function(){
         var slideno = $(this).data('slide');
         $('.slick-carousel').slick('slickGoTo', slideno - 1);
       });
+
+    window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.header__nav'),
+        menuItem = document.querySelectorAll('.header__link'),
+        hamburger = document.querySelector('.hamburger');
+    
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header__nav_active');
+        });
+    
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('header__nav_active');
+            });
+        });
+    });
   });
+
